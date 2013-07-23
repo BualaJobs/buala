b498451b8b21d629bb836c26701985eb2f2f950d
Paul Ballesty <paulballesty@gmail.com>
2013-07-23 19:02:48 -0300

    home controller with top ads (random)

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Buala::Application.routes.draw do
 
+  get "home/index"
+  root to: "home#index"
+  
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
 
   devise_for :users
-  ActiveAdmin.routes(self)
 
-  get "home/index"
-  root to: "home#index"
 end

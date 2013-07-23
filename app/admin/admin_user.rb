73d10f3d8e5d940f0fda28860f2d0c6a1002bda9
Paul Ballesty <paulballesty@gmail.com>
2013-07-23 19:01:27 -0300

    Customizing admin panels

--- app/admin/admin_user.rb
+++ app/admin/admin_user.rb
@@ -1,4 +1,7 @@
 ActiveAdmin.register AdminUser do     
+  
+  menu :priority => 2
+
   index do                            
     column :email                     
     column :current_sign_in_at        

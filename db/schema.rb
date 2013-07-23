810c5898c1832cad28ea4554dd6a1c41cee5ccf4
Paul Ballesty <paulballesty@gmail.com>
2013-07-23 19:12:18 -0300

    Adding published attribute to Advertisement

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130723030254) do
+ActiveRecord::Schema.define(:version => 20130723220822) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.string   "resource_id",   :null => false
@@ -52,6 +52,7 @@ ActiveRecord::Schema.define(:version => 20130723030254) do
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
     t.integer  "company_id"
+    t.boolean  "published"
   end
 
   create_table "companies", :force => true do |t|

68ff7dba2c65f9ac6851542c6a819359dc067380
Paul Ballesty <paulballesty@gmail.com>
2013-07-24 22:14:58 -0300

    Adding category seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,10 @@
 AdminUser.create email: 'admin@bualajobs.com', password: '123123123', password_confirmation: '123123123'
+
+Category.create name: 'Derecho'
+Category.create name: 'Agro'
+Category.create name: 'Animales'
+Category.create name: 'Arquitectura'
+Category.create name: 'Arte'
+Category.create name: 'Biotecnología y Farmacéutica'
+Category.create name: 'Negocios y Finanzas'
+# Poner todas...

5b7f161fa0e7627b3586d64fccc8d3d88e3d9f81
Paul Ballesty <paulballesty@gmail.com>
2013-07-24 23:44:50 -0300

    Adding company_type to companies

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,6 +1,8 @@
 class Company < ActiveRecord::Base
-  attr_accessible :description, :location, :name, :website, :category, :category_id
+  attr_accessible :description, :location, :name, :website, :category, :category_id, :company_type, 
+  	:company_type_id
   belongs_to :category
+  belongs_to :company_type
 
   validates :name, :description, :category, :presence => true
 

01649eedd456be3a1d695daf37990f91a456ca9d
Paul Ballesty <paulballesty@gmail.com>
2013-07-24 23:19:30 -0300

    Updating advertisement admin panel with categories

--- app/admin/advertisements.rb
+++ app/admin/advertisements.rb
@@ -22,9 +22,22 @@ ActiveAdmin.register Advertisement do
 	show do
 		attributes_table do
 			row :title
-			row :description
 			row :company
+			row :category_1
+			row :category_2
+			row :description
 		end
 	end
+
+	form do |f|
+    f.inputs do
+      f.input :title
+      f.input :company
+      f.input :category_1
+      f.input :category_2
+      f.input :description
+    end
+    f.actions
+  end
   
 end

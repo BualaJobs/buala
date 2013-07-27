bdd58d4cf38472fdc2d2f5d7e8795752a8e0c386
Juanjo <juanjo@ubuntu.(none)>
2013-07-27 19:35:41 -0300

    Added name to User on db and model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,11 +2,10 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   
-  attr_accessible :email, :password, :password_confirmation, :remember_me, :university_id, :degree_id
+  attr_accessible :email, :name, :password, :password_confirmation, :remember_me, :university_id, :degree_id
 
   belongs_to :university
   belongs_to :degree
 
-  validates :university, :degree, presence: true
-
+  validates :university, :degree, :name, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130726032346) do
+ActiveRecord::Schema.define(:version => 20130727215854) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.string   "resource_id",   :null => false
@@ -115,6 +115,7 @@ ActiveRecord::Schema.define(:version => 20130726032346) do
     t.datetime "updated_at",                             :null => false
     t.integer  "university_id"
     t.integer  "degree_id"
+    t.string   "name"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true

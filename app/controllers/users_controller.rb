8aca18f64a8d8c3d2b9083ecb9a2d1f1c405183c
Juanjo <juanjo@ubuntu.(none)>
2013-07-28 19:19:31 -0300

    Created UsersController, routed action show to /users/[:id]

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,9 +2,6 @@ class UsersController < ApplicationController
   
   def show
   	@user = User.find(params[:id])
-  	unless user_signed_in? && current_user.id == @user.id
-		render status: :forbidden, text: 'Forbidden'
-	end
   end
   
 end

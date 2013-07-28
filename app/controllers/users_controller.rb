bf117603b7d29142924ce5a11ab84035b1bc91d9
Juanjo <juanjo@ubuntu.(none)>
2013-07-28 19:43:15 -0300

    Cannot access another user profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,9 @@ class UsersController < ApplicationController
   
   def show
   	@user = User.find(params[:id])
+  	unless user_signed_in? && current_user.id == @user.id
+		render status: :forbidden, text: 'Forbidden'
+	end
   end
   
 end

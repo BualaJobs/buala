1f201ca0074c4ebc0187f41529166112b32de2e9
Paul Ballesty <paulballesty@gmail.com>
2013-07-28 22:11:27 -0300

    Tests for company badges and refactor published

--- spec/factories/companies.rb
+++ spec/factories/companies.rb
@@ -4,6 +4,21 @@ FactoryGirl.define do
     "Test company #{n}"
   end
 
+  trait :startup do
+    name "Startup company"
+    company_type { FactoryGirl.create(:company_type, :startup_type) }
+  end
+
+  trait :business do
+    name "Business company"
+    company_type { FactoryGirl.create(:company_type, :business_type) }
+  end
+
+  trait :ong do
+    name "Ong company"
+    company_type { FactoryGirl.create(:company_type, :ong_type) }
+  end
+
   factory :company do
     name { generate(:company_name) }
     website "http://www.testcompany.com"
--- spec/factories/company_types.rb
+++ spec/factories/company_types.rb
@@ -4,6 +4,18 @@ FactoryGirl.define do
     "Test company type #{n}"
   end
 
+  trait :startup_type do
+  	name "Startup"
+  end
+
+  trait :business_type do
+  	name "Empresa"
+  end
+
+  trait :ong_type do 
+  	name "Ong"
+  end
+
   factory :company_type do
     name { generate(:company_type_name) }
   end

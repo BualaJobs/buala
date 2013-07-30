0d5fef6c076b9ca53e783db2d0cddc903c8b10b4
Paul Ballesty <paulballesty@gmail.com>
2013-07-29 23:29:31 -0300

    Adding advertisement and working turn relationship

--- app/models/advertisement.rb
+++ app/models/advertisement.rb
@@ -1,11 +1,13 @@
 class Advertisement < ActiveRecord::Base
   attr_accessible :description, :title, :company, :company_id, :published, :category_1_id, :category_1, 
-  	:category_2_id, :category_2, :advertisement_type, :advertisement_type_id, :is_paid
+  	:category_2_id, :category_2, :advertisement_type, :advertisement_type_id, :is_paid,
+    :working_turn, :working_turn_id
 
   belongs_to :company
   belongs_to :category_1, :class_name => 'Category'
   belongs_to :category_2, :class_name => 'Category'
   belongs_to :advertisement_type
+  belongs_to :working_turn
 
   validates :title, :company, :category_1, :advertisement_type, :description, presence: true
   validates_length_of :title, :maximum => 45

06a3adac609e11a35b7ba545001ae0d2b2e2ed45
Paul Ballesty <paulballesty@gmail.com>
2013-07-31 22:16:30 -0300

    Running migration

--- db/schema.rb
+++ db/schema.rb
@@ -117,9 +117,9 @@ ActiveRecord::Schema.define(:version => 20130731023029) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                             :null => false
     t.datetime "updated_at",                             :null => false
-    t.string   "name"
     t.integer  "university_id"
     t.integer  "degree_id"
+    t.string   "name"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true

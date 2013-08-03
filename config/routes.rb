b30192e6601c0cb2538c2ad105550af2d24b5890
Paul Ballesty <paulballesty@gmail.com>
2013-08-03 17:11:02 -0300

    Adding advertisement requirements field

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Buala::Application.routes.draw do
 
   # get "home/index"
   # root to: "home#index"
-<<<<<<< HEAD
 
   # devise_for :users
 
@@ -13,17 +12,8 @@ Buala::Application.routes.draw do
   end
 
   resources :users, only: [:show]
-=======
->>>>>>> Bugfix i18n, Removing not used routes (pre-alpha)
   
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
 
-<<<<<<< HEAD
-=======
-  # devise_for :users
-
-  resources :advertisements, only: [:show]
-
->>>>>>> Bugfix i18n, Removing not used routes (pre-alpha)
 end

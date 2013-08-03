6ea2e62dc61a1fcbc155e5b98f1b161b15b39440
Paul Ballesty <paulballesty@gmail.com>
2013-08-03 02:30:29 -0300

    Bugfix i18n, Removing not used routes (pre-alpha)

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Buala::Application.routes.draw do
 
-  get "home/index"
-  root to: "home#index"
+  # get "home/index"
+  # root to: "home#index"
   
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
 
-  devise_for :users
+  # devise_for :users
 
   resources :advertisements, only: [:show]
   resources :users, only: [:show]

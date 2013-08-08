6e5dd840bd6089a37fe0c5188fd17d0e2368ed9d
Paul Ballesty <paulballesty@gmail.com>
2013-08-07 22:29:03 -0300

    Allowing all routes for development

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,15 @@
 Buala::Application.routes.draw do
 
-  # get "home/index"
-  # root to: "home#index"
+  get "home/index"
+  root to: "home#index"
   
-  devise_for :admin_users, ActiveAdmin::Devise.config
-  ActiveAdmin.routes(self)
 
-  # devise_for :users
+  devise_for :users
 
   resources :advertisements, only: [:show]
   resources :users, only: [:show]
+  
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
 
 end

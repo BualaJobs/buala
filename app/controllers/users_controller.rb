7203735c7a399754415dee1ab922886ca9d6660b
Paul Ballesty <paulballesty@gmail.com>
2013-08-07 22:10:00 -0300

    Fixing some rebase errors

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,5 @@ class UsersController < ApplicationController
   	unless user_signed_in? && current_user.id == params[:id].to_i
 		render status: :forbidden, text: 'Forbidden'
 	end
-  	@user = User.find(params[:id])
   end
 end

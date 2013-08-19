08096c5ed9529e7394489e61fc7a890f0f417831
Paul Ballesty <paulballesty@gmail.com>
2013-08-19 03:40:43 -0300

    Settting config variables for resume uploading

--- app/models/application.rb
+++ app/models/application.rb
@@ -9,8 +9,8 @@ class Application < ActiveRecord::Base
   has_attached_file :resume, storage: :dropbox, :dropbox_credentials => Rails.root.join('config/dropbox.yml')
 
   validates :name, :email, :university, :advertisement, :resume, :degree, presence: true
-  validates_attachment :resume, presence: true, content_type: { content_type: ['application/msword', 'application/pdf'] },
-    size: { in: 0..500.kilobytes }
+  validates_attachment :resume, presence: true, content_type: { content_type: ['application/msword', 'application/vnd.openxmlformats-officedocument.wordprocessingml.document', 'application/pdf'] },
+    size: { in: 0..1024.kilobytes }
   validates :email, uniqueness: {scope: :advertisement_id}
   validates_acceptance_of :accept_terms_and_conditions, accept: '1'
 

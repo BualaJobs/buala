46fdb951cc52e342cb2690ccf327237b2828b9d4
Paul Ballesty <paulballesty@gmail.com>
2013-10-14 01:05:26 -0300

    Removing old models

--- app/models/degree.rb
+++
@@ -1,9 +0,0 @@
-class Degree < ActiveRecord::Base
-  attr_accessible :name, :university
-
-  belongs_to :university
-
-  validates :name, :university, presence: true
-  validates_uniqueness_of :name
-
-end
